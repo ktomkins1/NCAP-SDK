b59c4f7be95a0aa7e577e5b33a7cc849120a69f1
James Ethridge <jeethridge@gmail.com>
2016-02-20 16:16:50 -0500

    Add example class to test travis and rtfd

--- ncaplite/ncaplite.py
+++ ncaplite/ncaplite.py
@@ -32,3 +32,49 @@ def my_stub():
     return 1
 
 
+class MyPublicClass(object):
+    """We use this as a public class example class.
+
+    You never call this class before calling :func:`mystub`.
+
+    .. note::
+
+       An example of intersphinx is this: you **cannot** use :mod:`pickle` on this class.
+
+    """
+
+    def __init__(self, foo, bar='baz'):
+        """A really simple class.
+
+        Args:
+           foo (str): We all know what foo does.
+
+        Kwargs:
+           bar (str): Really, same as foo.
+
+        """
+        self._foo = foo
+        self._bar = bar
+
+    def get_foobar(self, foo, bar=True):
+        """This gets the foobar
+
+        This really should have a full function definition, but I am too lazy.
+
+        >>> print get_foobar(10, 20)
+        30
+        >>> print get_foobar('a', 'b')
+        ab
+
+        Isn't that what you want?
+
+        """
+        return foo + bar
+
+    def _get_baz(self, baz=None):
+        """A private function to get baz.
+
+        This really should have a full function definition, but I am too lazy.
+
+        """
+        return baz
