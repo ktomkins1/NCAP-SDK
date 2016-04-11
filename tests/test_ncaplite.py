0b2e068a6406e3e6280fa67d9c7144cc7fd12e24
James Ethridge <jeethridge@asonunique.local>
2016-04-10 21:56:46 -0400

    update test config and tests to run against localhost inside vagrant box. should also work with travis config for #12

--- tests/test_ncaplite.py
+++ tests/test_ncaplite.py
@@ -14,13 +14,22 @@ from ncaplite import ncaplite
 from ncaplite import network_interface
 import time
 import xml.etree.ElementTree as ET
-import os
+
+
 class TestNcaplite(unittest.TestCase):
+    """This class defines the test runner for an NCAP instance.
+
+    We'll start with just this test class and then expand as modules are
+    added.
+    """
 
     def setUp(self):
-        pass
+        """Setup for NCAP unit tests"""
+        self.config_file_path = 'tests/testconfig.xml'
+        self.test_broker_ip = '127.0.0.1'
 
     def tearDown(self):
+        """Teardown for NCAP unit tests"""
         pass
 
     def test_ncap_can_register_network_if(self):
@@ -33,10 +42,10 @@ class TestNcaplite(unittest.TestCase):
         ncap_id = 12345
         jid = 'ncap@ncaplite.loc'
         password = 'mypassword'
-        broker_address=('10.10.100.4',5222)
+        broker_address = (self.test_broker_ip, 5222)
 
         ncap = ncaplite.NCAP(ncap_id)
-        network_if = network_interface.NetworkClient(jid,password,broker_address)
+        network_if = network_interface.NetworkClient(jid, password, broker_address)
         ncap.register_network_interface(network_if)
         ncap.start()
         time.sleep(1)
@@ -48,13 +57,13 @@ class TestNcaplite(unittest.TestCase):
         without specifying an explicit broker address
         :return:
         """
-        ncap_id=12345
+        ncap_id = 12345
         jid = 'ncap@ncaplite.loc'
         password = 'mypassword'
-        broker_address=('10.10.100.4',5222)
+        broker_address = (self.test_broker_ip, 5222)
 
         ncap = ncaplite.NCAP(ncap_id)
-        network_if = network_interface.NetworkClient(jid,password,broker_address)
+        network_if = network_interface.NetworkClient(jid, password, broker_address)
         ncap.register_network_interface(network_if)
         ncap.start()
         time.sleep(1)
@@ -62,40 +71,37 @@ class TestNcaplite(unittest.TestCase):
 
 
     def test_can_read_ncap_config_file(self):
-        print("\n My CWD: "+os.getcwd()+"\n")
-        tree = ET.parse('tests/testconfig.xml')
+        tree = ET.parse(self.config_file_path)
         root = tree.getroot()
 
-        roster_path=root.find('roster_path').text
-        assert(roster_path=='roster.xml')
-        broker_ip=root.find('broker_address').find('address').text
-        assert(broker_ip=='10.10.100.4')
-        broker_port=int(root.find('broker_address').find('port').text)
+        roster_path = root.find('roster_path').text
+        assert(roster_path == 'roster.xml')
+        broker_ip = root.find('broker_address').find('address').text
+        assert(broker_ip == self.test_broker_ip)
+        broker_port = int(root.find('broker_address').find('port').text)
         assert(broker_port == 5222)
-        broker_address=(broker_ip,broker_port)
-        jid=root.find('ncap_identification').find('jid').text
-        assert(jid=='ncap@ncaplite.loc')
-        password=root.find('ncap_identification').find('password').text
-        assert(password=='mypassword')
-        ncap_id=int(root.find('ncap_identification').find('ncap_id').text)
-        assert(ncap_id==12345)
-        ncap_type=root.find('ncap_identification').find('ncap_type').text
-        assert(ncap_type=='server')
-        ncap_model_number=int(root.find('ncap_identification').find('model_number').text)
-        assert(ncap_model_number==11111)
-        ncap_serial_number=int(root.find('ncap_identification').find('serial_number').text)
-        assert(ncap_serial_number==22222)
-        ncap_mfcr_id=int(root.find('ncap_identification').find('manufacturer_id').text)
-        assert(ncap_mfcr_id==33333)
+        jid = root.find('ncap_identification').find('jid').text
+        assert(jid == 'ncap@ncaplite.loc')
+        password = root.find('ncap_identification').find('password').text
+        assert(password == 'mypassword')
+        ncap_id = int(root.find('ncap_identification').find('ncap_id').text)
+        assert(ncap_id == 12345)
+        ncap_type = root.find('ncap_identification').find('ncap_type').text
+        assert(ncap_type == 'server')
+        ncap_model_number = int(root.find('ncap_identification').find('model_number').text)
+        assert(ncap_model_number == 11111)
+        ncap_serial_number = int(root.find('ncap_identification').find('serial_number').text)
+        assert(ncap_serial_number == 22222)
+        ncap_mfcr_id = int(root.find('ncap_identification').find('manufacturer_id').text)
+        assert(ncap_mfcr_id == 33333)
 
         ncap = ncaplite.NCAP(ncap_id)
-        ncap.load_config('tests/testconfig.xml')
-        network_if = network_interface.NetworkClient(ncap.jid,ncap.password,(ncap.broker_ip,ncap.broker_port))
+        ncap.load_config(self.config_file_path)
+        network_if = network_interface.NetworkClient(ncap.jid, ncap.password, (ncap.broker_ip, ncap.broker_port))
         ncap.register_network_interface(network_if)
         ncap.start()
         time.sleep(1)
         ncap.stop()
-
 
 
 if __name__ == '__main__':
