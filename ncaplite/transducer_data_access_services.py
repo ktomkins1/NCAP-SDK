f3ba4f8b38a8800f7bd9e5d5a93f251826cccd03
James Ethridge <jeethridge@asonunique.local>
2016-04-28 22:40:30 -0400

    add implementation of read_transducer_block_data

--- ncaplite/transducer_data_access_services.py
+++ ncaplite/transducer_data_access_services.py
@@ -7,15 +7,24 @@
 
 """
 
+import time
+
 
 class TransducerDataAccessServices(object):
-    """TransducerDataAccessServices"""
+    """
+    Transducer Data Access Services for NCAP.
+
+    This class defines the Transducer Access Services
+    exposed by the NCAP.
+    """
+
     def __init__(self, name="Transducer Data Access Services"):
+        """Initialize the TransducerDataAccessServices object."""
         self.name = name
 
     def register_transducer_access_service(self, transducer_access):
-        """ registter a TimDiscovery service object with
-            the TransducerDataAccessServices object."""
+        """Register a TimDiscovery service object with the\
+        TransducerDataAccessServices object."""
         self.transducer_access = transducer_access
 
     def read_transducer_sample_data_from_a_channel_of_a_tim(self,
@@ -31,13 +40,63 @@ class TransducerDataAccessServices(object):
         Args:
             ncap_id: ID of the NCAP application being queried
             tim_id: ID of the TIM being queried
-            channel_id:
+            channel_id: the channel ID of the TIM
+
+        Returns: A tuple containing the following:
+            error_code: an error code
+            ncap_id: the ncap id
+            tim_id: the id of the tim that was read
+            channel_id: the id of the channel read from the TIM
+            sample_data: the block of sample data given as a list
         """
-        data = []
+        sample_data = []
         trans_comm_id = self.transducer_access.open(tim_id, channel_id)
         self.transducer_access.read_data(trans_comm_id, timeout,
-                                         sampling_mode, data)
+                                         sampling_mode, sample_data)
         self.transducer_access.close(trans_comm_id)
         error_code = 0
 
-        return (error_code, ncap_id, tim_id, channel_id, data)
+        return (error_code, ncap_id, tim_id, channel_id, sample_data)
+
+    def read_transducer_block_data_from_a_channel_of_a_tim(self,
+                                                           ncap_id,
+                                                           tim_id,
+                                                           channel_id,
+                                                           timeout,
+                                                           number_of_samples,
+                                                           sample_interval,
+                                                           start_time):
+        """Read a block of sensor data from a channel of a TIM
+        at a specified interval.
+
+        Args:
+            ncap_id: the ncap ID
+            tim_id: the ID of the TIM to read from
+            channel_id: the channel ID of the TIM
+            timeout: a timeout value for the read attempt
+            number_of_samples: the number of samples to read
+            sample_interval: the desired interval between each sample
+            start_time: the start time to begin reading
+
+        Returns: A tuple containing the following:
+            error_code: an error code
+            ncap_id: the ncap id
+            tim_id: the id of the tim that was read
+            channel_id: the id of the channel read from the TIM
+            transducer_block_data: the block of sample data given as a list
+        """
+        error_code = 0
+        sampling_mode = 5
+        transducer_block_data = []
+        args = (ncap_id, tim_id, channel_id, timeout, sampling_mode)
+
+        time.sleep(start_time)
+
+        for _ in range(number_of_samples):
+            result = \
+                self.read_transducer_sample_data_from_a_channel_of_a_tim(*args)
+            if(result[0] != 0):
+                error_code = 1
+            transducer_block_data.append(result[4][0])
+            time.sleep(sample_interval)
+        return (error_code, ncap_id, tim_id, channel_id, transducer_block_data)
--- tests/test_transducer_data_access_services.py
+++ tests/test_transducer_data_access_services.py
@@ -10,6 +10,7 @@ Tests for `transducer_data_access_services` module.
 
 import unittest
 import mock
+import time
 from ncaplite import transducer_data_access_services
 from ncaplite import transducer_services_base
 
@@ -62,4 +63,54 @@ class TestTransducerDataAccessServices(unittest.TestCase):
 
         tdaccs.open.assert_called_with(01, 01)
         tdaccs.read_data.assert_called_with(1, 100, 0, mock.ANY)
+        self.assertEqual(expected_response, response)
+
+    def test_read_transducer_block_data_from_a_channel_of_a_tim(self):
+        """ Test reading transducer block data channel of a tim. """
+
+        def open_mock(tim_id, channel_id):
+            trans_comm_id = 1
+            return trans_comm_id
+
+        def read_data_mock(trans_comm_id, timeout,
+                           sampling_mode, result):
+            result.append(read_data_mock.count)
+            read_data_mock.count = read_data_mock.count+1
+        read_data_mock.count = 1024
+
+        tdaccs = mock.Mock(spec=transducer_services_base.TransducerAccessBase)
+        tdaccs.open.side_effect = open_mock
+        tdaccs.read_data.side_effect = read_data_mock
+
+        tdas = transducer_data_access_services.TransducerDataAccessServices()
+        tdas.register_transducer_access_service(tdaccs)
+
+        request = {
+                   'ncap_id': 1234,
+                   'tim_id': 01,
+                   'channel_id': 02,
+                   'timeout': 100,
+                   'number_of_samples': 3,
+                   'sample_interval': 1,
+                   'start_time': 0
+                }
+
+        expected_response = (0, 1234, 1, 2, [1024, 1025, 1026])
+        t_expected = 3
+
+        t_start = time.time()
+        response = tdas.read_transducer_block_data_from_a_channel_of_a_tim(
+                                                request['ncap_id'],
+                                                request['tim_id'],
+                                                request['channel_id'],
+                                                request['timeout'],
+                                                request['number_of_samples'],
+                                                request['sample_interval'],
+                                                request['start_time']
+                                                )
+        t_end = time.time()
+        t_actual = int(t_end - t_start)
+        tdaccs.open.assert_called_with(01, 02)
+        tdaccs.read_data.assert_called_with(1, 100, 5, mock.ANY)
+        self.assertEqual(t_expected, t_actual)
         self.assertEqual(expected_response, response)
